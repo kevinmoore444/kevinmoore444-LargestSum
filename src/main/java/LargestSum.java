33d7cea9c8e5a7185134584d0ed89db1510031f3
Kevin Moore <kevinmoore444@gmail.com>
2023-07-11 19:49:04 +0000

    Completed LargestSum Exercise

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -10,6 +10,25 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int max1 = Integer.MIN_VALUE;
+        int max1Index = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        //Loop through the list to find the largest value and store it as max1
+        for(int i=0; i<nums.size(); i++){
+            if(nums.get(i)> max1){
+                max1 = nums.get(i);
+                max1Index = i;
+            }
+        }
+        //Remove that largest value from the list
+        nums.remove(max1Index);
+        //Loop through the list to find the largest remaining value and store it as max2
+        for(int i=0; i<nums.size(); i++){
+            if(nums.get(i)> max2){
+                max2 = nums.get(i);
+            }
+        }
+        return max1+max2;
     }
 }
